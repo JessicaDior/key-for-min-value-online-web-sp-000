93ea9c2d93d395577a8c847bbf6c552de75c6126
Jc Dior <jessicaclairdior@gmail.com>
2020-09-07 02:11:59 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.each do |a, b|
-    if a > b
-      return a
+  name_hash.sort do |a, b|
+    a <=> b
+    return a
     end
-    end
+  
 end
