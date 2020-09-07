9e9feb9557fcaf8892989fc5fe84fd0760d11303
Jc Dior <jessicaclairdior@gmail.com>
2020-09-07 02:27:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,14 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-min_value = 0
-key_for_min_value = nil
-
-name_hash.each do |key, value|
-  if value > min_value
-    min_value = value
-    key_for_min_value = key
-    puts key
-  end
-  end
+  lowest_key = nil 
+  lowest_value = nil 
+  name_hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v 
+      lowest_key = k 
+    end 
+  end 
+  lowest_key
+end 
 end
